12eecd43008f0c0b7bbc389ae5a5c28763aff6f8
Apoorva Teli <apoorvateli@gmail.com>
2018-03-10 20:49:27 +0530

    Add body-parser package, setup contact form and POST route

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require("express"),
 app = express(),
-mongoose = require("mongoose");
+mongoose = require("mongoose"),
+bodyParser= require("body-parser");
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost/contacts", {useMongoClient: true});
+mongoose.connect("mongodb://localhost/contacts");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -16,25 +18,41 @@ var contactSchema = new mongoose.Schema({
 
 var Contact = mongoose.model("Contact", contactSchema);
 
-Contact.create(
-  {
-    name: "Test name",
-    email: "test@test.com",
-    message: "test message"
-  },
-  function(err, newContact) {
+// Contact.create(
+//   {
+//     name: "Test name",
+//     email: "test@test.com",
+//     message: "test message"
+//   },
+//   function(err, newContact) {
+//     if (err) {
+//       console.log("ERROR while creating new contact message");
+//     }
+//     else {
+//       console.log("NEW contact added to DB:");
+//       console.log(newContact);
+//     }
+//   }
+// );
+
+// Display landing page with contact form
+app.get("/", function(req, res) {
+  res.render("landing");
+});
+
+// CREATE - Create a new contact in the DB
+app.post("/createContact", function(req, res) {
+  Contact.create(req.body.contact, function(err, newContact) {
     if (err) {
+      res.render("landing");
       console.log("ERROR while creating new contact message");
     }
     else {
+      res.redirect("/"); // go to landing page
       console.log("NEW contact added to DB:");
       console.log(newContact);
     }
-  }
-);
-
-app.get("/", function(req, res) {
-  res.render("landing");
+  });
 });
 
 app.listen(3000, function() {
