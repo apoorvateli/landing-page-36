2d59af8d06c47075c53a06dbcebe80b29c079b2d
Apoorva Teli <apoorvateli@gmail.com>
2018-03-10 21:19:25 +0530

    Sanitize the inputs coming from contact form using express-sanitizer package

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express"),
 app = express(),
 mongoose = require("mongoose"),
-bodyParser= require("body-parser");
+bodyParser= require("body-parser"),
+expressSanitizer = require("express-sanitizer");
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/contacts");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -18,23 +20,6 @@ var contactSchema = new mongoose.Schema({
 
 var Contact = mongoose.model("Contact", contactSchema);
 
-// Contact.create(
-//   {
-//     name: "Test name",
-//     email: "test@test.com",
-//     message: "test message"
-//   },
-//   function(err, newContact) {
-//     if (err) {
-//       console.log("ERROR while creating new contact message");
-//     }
-//     else {
-//       console.log("NEW contact added to DB:");
-//       console.log(newContact);
-//     }
-//   }
-// );
-
 // Display landing page with contact form
 app.get("/", function(req, res) {
   res.render("landing");
@@ -42,6 +27,9 @@ app.get("/", function(req, res) {
 
 // CREATE - Create a new contact in the DB
 app.post("/createContact", function(req, res) {
+  // sanitize the data coming from the contact form
+  req.body.contact = req.sanitize(req.body.contact);
+  // create contact
   Contact.create(req.body.contact, function(err, newContact) {
     if (err) {
       res.render("landing");
