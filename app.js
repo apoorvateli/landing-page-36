7af3cc48bf69a25973299cacb4bda92903ac5ec3
Apoorva Teli <apoorvateli@gmail.com>
2018-03-10 20:14:57 +0530

    Config MongoDB and Mongoose to store contacts

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ mongoose.connect("mongodb://localhost/contacts", {useMongoClient: true});
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// MONGOOSE CONFIG SCHEMA SETUP
+var contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String
+});
+
+var Contact = mongoose.model("Contact", contactSchema);
+
+Contact.create(
+  {
+    name: "Test name",
+    email: "test@test.com",
+    message: "test message"
+  },
+  function(err, newContact) {
+    if (err) {
+      console.log("ERROR while creating new contact message");
+    }
+    else {
+      console.log("NEW contact added to DB:");
+      console.log(newContact);
+    }
+  }
+);
+
 app.get("/", function(req, res) {
   res.render("landing");
 });
